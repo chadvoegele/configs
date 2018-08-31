d36619e4f61ca33a30bd14ecaa13a4621a6821cb
Chad Voegele <cavoegele@gmail.com>
2018-08-30 20:34:15 -0700

    Check for gpg-agent before calling.

--- any/.config/zsh/zsh.conf.d/72-gpg-agent.zsh
+++ any/.config/zsh/zsh.conf.d/72-gpg-agent.zsh
@@ -1,4 +1,8 @@
 export GPG_TTY=$(tty)
 
-# Refresh gpg-agent tty in case user switches into an X session
-gpg-connect-agent updatestartuptty /bye >/dev/null
+command -v gpg-connect-agent
+if [[ $? -eq 0 ]]
+then
+  # Refresh gpg-agent tty in case user switches into an X session
+  gpg-connect-agent updatestartuptty /bye >/dev/null
+fi
