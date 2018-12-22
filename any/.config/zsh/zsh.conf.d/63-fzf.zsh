653cfc79a7bfb72fbe469b691eef0f31e2c39e85
Chad Voegele <cavoegele@gmail.com>
2018-12-22 10:24:32 -0800

    Ignore case in fzf fix

--- any/.config/zsh/zsh.conf.d/63-fzf.zsh
+++ any/.config/zsh/zsh.conf.d/63-fzf.zsh
@@ -1,3 +1,3 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-export FZF_DEFAULT_COMMAND='rg -i --color=never -l ""'
-export FZF_DEFAULT_OPTS='--bind=:abort'
+export FZF_DEFAULT_COMMAND='rg --color=never -l ""'
+export FZF_DEFAULT_OPTS='-i --bind=:abort'
