b33564efb74956ea239a62a84440e7e4f50bf2ae
Chad Voegele <cavoegele@gmail.com>
2019-08-16 09:41:02 -0700

    Switch to sway

--- any/.config/zsh/zsh.conf.d/70-startx.zsh
+++ any/.config/zsh/zsh.conf.d/70-startx.zsh
@@ -1,6 +1,6 @@
 # Start X if it is vc1 and X hasn't been started already
 if [ -z "$DISPLAY" ] && [ $(tty) = /dev/tty1 ] && [ $(whence startx) ]
 then
-  startx -- -dpi 180 -ardelay 250 -arinterval 20 vt1
+  sway
   logout
 fi
