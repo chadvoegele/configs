9303de084537668caee7a9dd73af876fcb8f1097
Chad Voegele <cavoegele@gmail.com>
2019-11-28 05:09:36 -0800

    startx starts sway

--- any/.config/zsh/zsh.conf.d/70-startx.zsh
+++ any/.config/zsh/zsh.conf.d/70-startx.zsh
@@ -1,6 +1,7 @@
 # Start X if it is vc1 and X hasn't been started already
-if [ -z "$DISPLAY" ] && [ $(tty) = /dev/tty1 ] && [ $(whence startx) ]
+if [ -z "$WAYLAND_DISPLAY" ] && [ $(tty) = /dev/tty1 ] && [ $(whence sway) ]
 then
+  WAYLAND_DISPLAY="wayland-0"
   sway
   logout
 fi
