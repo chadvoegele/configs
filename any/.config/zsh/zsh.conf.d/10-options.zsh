06564d0fe3cb0ec9cf462d54befe308bd760a720
Chad Voegele <chad@voegele.me>
2023-05-21 06:37:05 -0500

    Add less pager to zsh

--- any/.config/zsh/zsh.conf.d/10-options.zsh
+++ any/.config/zsh/zsh.conf.d/10-options.zsh
@@ -5,6 +5,7 @@ WORDCHARS='*?[]~=&;!#$%^(){}-'
 
 export DE="generic"
 export EDITOR="nvim"
+export PAGER="less"
 export LS_COLORS='rs=0:di=34:ln=36:mh=00:pi=40;33:so=35:do=35:bd=40;33:cd=40;33:or=40;31:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=32'
 
 setopt no_flow_control
