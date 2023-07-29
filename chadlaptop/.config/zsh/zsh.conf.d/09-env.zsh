1a9fa93d2df31c105d239a485ef4c0d240183b73
Chad Voegele <chad@voegele.me>
2023-07-29 12:25:57 -0500

    New printer

--- chadlaptop/.config/zsh/zsh.conf.d/09-env.zsh
+++ chadlaptop/.config/zsh/zsh.conf.d/09-env.zsh
@@ -1,4 +1,4 @@
 export BROWSER="firefox"
 export PASSWORD_STORE_DIR="${HOME}/code/passwords"
 export GDK_BACKEND="wayland"
-export PRINTER="HP_LaserJet_1022"
+export PRINTER="HP_LaserJet_Pro_3001dw"
