9df643bc959583771876bde8b622f971b022736a
Chad Voegele <chad@voegele.me>
2024-08-08 23:38:30 -0500

    Fix use-package in emacs.d/init.el

--- any/.emacs.d/init.el
+++ any/.emacs.d/init.el
@@ -2,10 +2,6 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
 (setq quelpa-update-melpa-p nil)
 (unless (package-installed-p 'quelpa)
   (with-temp-buffer
@@ -13,6 +9,7 @@
     (eval-buffer)
     (quelpa-self-upgrade)))
 
+(require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
 (use-package evil)
