d1692954eb72bfac84fc02b3bb1342709f38d062
Chad Voegele <chad@voegele.me>
2026-07-12 06:09:30 -0500

    Switch to environment.d

--- chaddev.lan/.config/zsh/zsh.conf.d/09-env.zsh
+++
@@ -1,1 +0,0 @@
-export PASSWORD_STORE_DIR="${HOME}/code/code.voegele.me/passwords/"
--- chadlaptop.lan/.config/zsh/zsh.conf.d/09-env.zsh
+++
@@ -1,4 +0,0 @@
-export BROWSER="firefox"
-export PASSWORD_STORE_DIR="${HOME}/code/passwords"
-export GDK_BACKEND="wayland"
-export PRINTER="HP_LaserJet_Pro_3001dw"
